847c65ac177102dc0a0a6d52928616ca6b4bf66a
RobertoGajardoR <robertogajardo.r@gmail.com>
2016-06-24 17:42:54 -0300

    Update libreria.php

--- libreria.php
+++ libreria.php
@@ -33,7 +33,7 @@ if (@!$_SESSION['user']) {
             <span class="icon-bar"></span>
           </button>
           <!-- <a class="navbar-brand" href="#">Libreria</a> -->
-          <a class='navbar-brand' href='index.php'>
+          <a class='navbar-brand' href='libreria.php'>
           <img id='imghe' class='img-rounded' src='medios/logo.jpg' alt=''></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
