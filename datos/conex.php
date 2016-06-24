a4979a4be6499d56f6fc49bfb61fe0b8dd8fca30
RobertoGajardoR <robertogajardo.r@gmail.com>
2016-06-24 16:08:58 -0300

    Update conex.php

--- datos/conex.php
+++ datos/conex.php
@@ -1,9 +1,9 @@
 <?php 
 	
-define('DB_SERVER','localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'libreria');
+define('DB_SERVER','br-cdbr-azure-south-b.cloudapp.net');
+define('DB_USERNAME', 'b68002cf79a115');
+define('DB_PASSWORD', 'a1c5ccdf');
+define('DB_DATABASE', 'itclibreria');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
